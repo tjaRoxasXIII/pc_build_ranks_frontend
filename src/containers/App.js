8d812fe3b988c188945b36594373e3180efe9e2e
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-21 15:34:45 -0700

    did a yarn add to add styled componenets

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Container, } from "semantic-ui-react";
 import Signup from '../components/Signup'
+import Home from './Home';
 
 
 function App() {
   return (
     <Container style={{ padding: "30px 0" }}>
-      <Signup />
+      {/* <Signup /> */}
+      <Home />
     </Container>
   );
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,20 +1,21 @@
-import React, { useContext, useEffect } from 'react';
-import styled from "styled-components";
-;
+import React from 'react';
+import styled from 'styled-components';
 
 
-  return (
-    <Navbar>
-  
-    </Navbar>
-  )
+ const Home = () => {
+   return (
+     <Navbar>
+       <h1>This is a test</h1>
+     </Navbar>
+   );
+ };
 
 
 const Navbar = styled.div`
   margin-top: -3.35rem;
   padding: 1rem 0;
   font-size: 18px;
-  color: white;
+  color: black;
   font-weight: 400;
 `
 
