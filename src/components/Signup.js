cb8439f4f714407675c8324d9e8f8b18c9a5891b
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-21 14:00:14 -0700

    just changed some syntax formatting

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -19,7 +19,7 @@ export default function Signup() {
       e.preventDefault()
       fetch("http://localhost:3000/users", {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
       })
         .then(response => response.json())
