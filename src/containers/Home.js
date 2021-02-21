a011486caf40848f3cac6ebefb1711c6d2e82c0f
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-21 15:05:11 -0700

    starting home page not much yet

---
+++ src/containers/Home.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from 'react';
+import styled from "styled-components";
+;
+
+
+  return (
+    <Navbar>
+  
+    </Navbar>
+  )
+
+
+const Navbar = styled.div`
+  margin-top: -3.35rem;
+  padding: 1rem 0;
+  font-size: 18px;
+  color: white;
+  font-weight: 400;
+`
+
+export default Home;
