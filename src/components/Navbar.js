458acfe000b4d9ebcf26ba57ed9cf67a70e10e17
Daniel McKenna <mckenna.daniel15@gmail.com>
2021-02-23 20:04:55 -0700

    finished the navbar I still want to make changes later but i am satisfied its working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 
 
  function Navbar() {
+   const [isOpen, setIsOpen] = useState (false)
    
    return (
        <div className="nav">
-        <div className="nav-links-left">
-          <ul>
-            <li>Home</li>
-          </ul>
+        <div className="nav-link-left">
+          <i className="fas fa-keyboard logo"></i>
+          <i className="fas fa-mouse-pointer"></i>
+          <a href="#" className="home-logo">Home</a>
         </div>
-        <div className="hamburger">
+        <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
           <span />
           <span />
           <span />
         </div>
-        <div className="nav-links-right">
-          <li>Register</li>
-          <li>Login</li>
-
-        </div>
-        <div className="nav-menu">
-          <ul>
-            <li>Profile</li>
-          </ul>
-
-        </div>
+        <Menu isOpen={isOpen }>
+          <a href="#" className="menu-link">Register</a>
+          <a href="#" className="menu-link">Login</a>
+          <a href="#" className="menu-link">Profile</a>
+          <a href="#" className="menu-link">PC builds</a>
+        </Menu>
        </div>
        
    );
  };
+
+ const Menu = styled.div`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ position: relative;
+
+@media (max-width: 768px) {
+   overflow: hidden;
+   flex-direction: column;
+   width: 100%;
+   max-height: ${({isOpen}) => (isOpen ? "300px" : "0px")};
+   transition: max-height: 0.3 ease-in;
+   background-color: #618992;
+}
+ `
 
 export default Navbar;
