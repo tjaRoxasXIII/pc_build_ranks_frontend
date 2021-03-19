e44573c0118ef49fade02df99464a34cdb7bd56b
Taylor <tjaroguetech@gmail.com>
2021-03-19 14:30:46 -0400

    Working on Auth fixes

--- src/components/Builds.js
+++ src/components/Builds.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import PersonalBuildsContainer from './PersonalBuilds/PersonalBuildsContainer'
+import { useSelector } from 'react-redux'
 
 export default function Builds() {
+    const userId = useSelector(state => state.userId)
     const displayForm = () => {
         let form = document.getElementById("new-pc-form")
         form.style.display = "block"
+    }
+
+    function handleOnSubmit(e) {
+        let newComputer = {
+            name: e.target[1].value,
+            CPU: parseInt(e.target[2].value),
+            GPU: parseInt(e.target[3].value),
+            user_id: userId
+        }
+        debugger
+        e.preventDefault()
+        fetch('http://localhost:3000/computers', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+         },
+            body: JSON.stringify(newComputer)
+        })
+
     }
 
     return(
@@ -13,7 +35,7 @@ export default function Builds() {
         
         <PersonalBuildsContainer />
         <h2>Add A Build <button onClick={displayForm}>+</button></h2>
-        <form id="new-pc-form" style={{display: 'none'}}>
+        <form id="new-pc-form" onSubmit={handleOnSubmit} style={{display: 'none'}}>
             <label>Image</label>
             <input type="file"></input><br/>
             <label>Name</label>
